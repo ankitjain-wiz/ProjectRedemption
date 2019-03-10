c295b917e214aab41e18004d301f4276df7dfd0e
ankitjain <ankitjain19962013@gmail.com>
2019-03-10 20:46:31 +0530

    backend stack

---
+++ MicroserviceStack/src/main/java/com/micro/stack/MicroserviceStackApplication.java
@@ -0,0 +1,13 @@
+package com.micro.stack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceStackApplication.class, args);
+	}
+
+}

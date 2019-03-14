17fc1da40ec9b42d8bc42c39c9d557217a6c5cbd
Ankit Jain <ankit.jain42@soprasteria.com>
2019-03-14 10:29:00 +0530

    Eureka - complete

--- Rainbow/src/main/java/com/ankit/rainbow/MessageRestController.java
+++ Rainbow/src/main/java/com/ankit/rainbow/MessageRestController.java
@@ -1,9 +1,12 @@
 package com.ankit.rainbow;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@RefreshScope
 @RestController
 class MessageRestController {
  
